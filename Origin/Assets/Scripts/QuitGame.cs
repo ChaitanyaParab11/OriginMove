7d28ce62b116ee760bf3a4f17bab622adf954502
Chaitanya Parab <chaitanyaparab111@gmail.com>
2023-08-16 15:57:45 +0530

    Final Game

---
+++ Origin/Assets/Scripts/QuitGame.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class QuitGame : MonoBehaviour
+{
+    public void EndGame()
+    {
+        SceneManager.LoadScene("Menu");
+        Debug.Log("QUIT!");
+    }
+}
